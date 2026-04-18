0886d1e607beaa3cc934ecfeb445bbc1b5517b03
ggegrampeadores <ggegrampeadores@gmail.com>
2026-04-18 12:30:18 -0300

    refactor: add config.js with centralized Supabase credentials

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuracao centralizada do Supabase - Controle Total GGE
+// Importar este arquivo ANTES de qualquer script que use Supabase
+(function() {
+  window.GGE_SUPABASE_URL = 'https://opceujqpqyvxsatzdarg.supabase.co';
+  window.GGE_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wY2V1anFwcXl2eHNhdHpkYXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NzIwMDEsImV4cCI6MjA4OTA0ODAwMX0.XKhgaNBJF8xiHiGEWEs8HL1dq5KFJpo2_RQckjsY9kc';
+})();
